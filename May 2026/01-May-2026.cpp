47f6405eb701d25f2b08ea948557c3e774f63c8b
shadab41 <shadabjhu00447@gmail.com>
2026-05-02 10:11:50 +0530

    Day 1

---
+++ May 2026/01-May-2026.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//396. Rotate Function
+//T.c : O(n)
+//S.C : o(1)
+
+
+
+class Solution {
+public:
+    int maxRotateFunction(vector<int>& nums) {
+
+        int n = nums.size();
+
+        int sum = 0;
+        int F = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            sum += nums[i];
+            F += i*nums[i];
+        }
+
+        int result = F;
+
+        for(int k = 0; k<=n-1; k++){
+            int newF = F + sum - n*nums[n-1-k];
+
+            result = max(result, newF);
+
+            F = newF;
+        }
+
+        return result;
+        
+        
+        
+    }
+};
