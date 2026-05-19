a5b175885831b04b161bcedffbc7f8ee1cd643b3
shadab41 <shadabjhu00447@gmail.com>
2026-05-19 23:41:44 +0530

    LC-2540

--- May 2026/19-May-2026.cpp
+++ May 2026/19-May-2026.cpp
@@ -1,5 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// LC - 1306
+// LC - 2540
+
+class Solution {
+public:
+    int getCommon(vector<int>& nums1, vector<int>& nums2) {
+        
+        int i = 0, j = 0;
+
+        while(i < nums1.size() && j < nums2.size()) {
+
+            if(nums1[i] == nums2[j]) {
+                return nums1[i];
+            }
+
+            else if(nums1[i] < nums2[j]) {
+                i++;
+            }
+
+            else {
+                j++;
+            }
+        }
+
+        return -1;
+    }
+};
 
